235c7cbdac3299e78f29582c255cf2e4ce2cfdcd
Nethra Harini M <nethraharini0904@gmail.com>
2024-11-03 23:31:10 +0530

    intro-if-else.java

--- intro-if-else.java
+++ intro-if-else.java
@@ -15,13 +15,18 @@ public class Solution {
     public static void main(String[] args) {
         int N = scanner.nextInt();
         scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
-           if(N % 2 == 0){
-              System.out.println("Not Weird");
+           if (N % 2 != 0) {
+            System.out.println("Weird");
+        } else { // N is even
+            if (N >= 2 && N <= 5) {
+                System.out.println("Not Weird");
+            } else if (N >= 6 && N <= 20) {
+                System.out.println("Weird");
+            } else if (N > 20) {
+                System.out.println("Not Weird");
             }
-           else{
-              System.out.println("Weird");
-             }
-      
+        }
+          
         
         scanner.close();
     }
